27c78c08b1e4e2b3c40bc9222c35d38f727903cf
John-Michael Glenn <phyre19@gmail.com>
2023-08-03 16:50:53 -0700

    Version 1.0.2
    Moved script from loading via defer to being async and waiting for window.onload to fire before app logic begins.
    
    Updated UPC verification method.
    
    Made indentation consistent as tabs across the project.
    
    Changed instances of torch to light in CSS.

--- app.js
+++ app.js
@@ -1,296 +1,322 @@
-(function(){
-  if (!window.BarcodeDetector) {
-    alert("BarcodeDetector API not found");
-    return;
-  }
+window.addEventListener("load", event => {
+	if (!window.BarcodeDetector) {
+		alert("BarcodeDetector API not found. Until the option is enabled, you will not be able to use this app. Please go to 'about://flags/#enable-experimental-web-platform-features' and enable the option, if available.");
+		return;
+	}
 
-  // Register the Service Worker, if able; then listen for app updates and prompt to upgrade
-  let iSW;
+	// Polyfill a "playing" property for media elements to easily check if they're actively playing
+	if (!HTMLMediaElement.hasOwnProperty("playing")) {
+		Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+			get: function() {
+				return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+			}
+		});
+	}
 
-  update_btn.addEventListener("click", () => iSW.postMessage({ action: "skipWaiting" }));
+	// Register the Service Worker, if able; then listen for app updates and prompt to upgrade
+	const ServiceWorker = {
+		agent: undefined,
+		refreshing: false
+	};
 
-  navigator.serviceWorker.register("sw.js", { scope: location.pathname })
-  .then(reg => {
-    reg.addEventListener("updatefound", () => {
-      iSW = reg.installing;
-      iSW.addEventListener("statechange", function() {
-        if (this.state !== "installed") return;
-        if (navigator.serviceWorker.controller) update_btn.hidden = false;
-      });
-    });
-  })
-  .catch(err => console.error(err));
+	update_btn.addEventListener("click", () => ServiceWorker.agent.postMessage({ action: "skipWaiting" }));
 
-  // Reload the page after the serviceWorker controller has changed to the latest version
-  let refreshingPage;
-  navigator.serviceWorker.addEventListener("controllerchange", event => {
-    if (refreshingPage) return;
-    location.reload();
-    refreshingPage = true;
-  });
+	navigator.serviceWorker.register("sw.js", { scope: location.pathname })
+	.then(reg => {
+		reg.addEventListener("updatefound", () => {
+			ServiceWorker.agent = reg.installing;
+			ServiceWorker.agent.addEventListener("statechange", function() {
+				if (this.state !== "installed") return;
+				if (navigator.serviceWorker.controller) update_btn.hidden = false;
+			});
+		});
+	})
+	.catch(err => console.error(err));
 
-  // Set variables
-  const height = screen.width,
-        width = height * (9 / 16),
-        constraints = {
-          video: {
-            aspectRatio: 9/16,
-            facingMode: "environment",
-            //width: 126, // this is actually the height, no idea why they are reversed...
-            height,
-            zoom: 3
-          },
-          audio: false
-        },
-        detector = new BarcodeDetector({
-          formats: ["upc_a", "upc_e", "qr_code"]
-        }),
-        inventory = {};
-  let track;
+	// Reload the page after the serviceWorker controller has changed to the latest version
+	navigator.serviceWorker.addEventListener("controllerchange", event => {
+		if (ServiceWorker.refreshing) return;
+		location.reload();
+		ServiceWorker.refreshing = true;
+	});
 
-  // Connect to the camera
-  navigator.mediaDevices.getUserMedia(constraints)
-  .then(stream => {
-    cam.style.width = height;
-    cam.style.height = width;
-    cam.style.marginLeft = `calc(50vw - ${height / 2}px)`;
-    track = stream.getVideoTracks()[0];
-    cam.srcObject = stream;
-    cam.autoplay = true;
-    scan_btn.disabled = false;
-    torch.disabled = false;
-  })
-  .catch(err => alert(err));
+	// Set variables
+	const height = screen.availWidth,
+				width = height * (9 / 16),
+				constraints = {
+					video: {
+						aspectRatio: 9/16,
+						facingMode: "environment",
+						//width: 126, // this is actually the height, no idea why they are reversed...
+						height,
+						zoom: 3
+					},
+					audio: false
+				},
+				detector = new BarcodeDetector({
+					formats: ["upc_a", "upc_e", "qr_code"]
+				}),
+				inventory = {};
+	let track, isStarted = false;
 
-  // Toggle the flashlight
-  torch.addEventListener("click", event => {
-    let target = event.target,
-        text = target.textContent;
+	document.addEventListener("visibilitychange", () => {
+		if (!isStarted) return;
+		if (document.visibilityState === "hidden") {
+			if (cam.playing) cam.pause();
+			return;
+		}
+		if (cam.paused) cam.play();
+	});
 
-    if (text === "Light On") {
-      // Turn light on
-      track.applyConstraints({
-        advanced: [{ torch: true }]
-      }).then(() => {
-        target.textContent = "Light Off";
-      }).catch(err => alert(err));
-    } else {
-      // Turn light off
-      track.applyConstraints({
-        advanced: [{ torch: false }]
-      }).then(() => {
-        target.textContent = "Light On";
-      }).catch(err => alert(err));
-    }
-  });
+	//TODO Use ImageCapture API to get an image to process instead of the raw cam feed
 
-  scan_btn.addEventListener("click", event => {
-    scan_btn.disabled = true;
-    scan();
-  });
+	// Connect to the camera
+	navigator.mediaDevices.getUserMedia(constraints)
+	.then(stream => {
+		track = stream.getVideoTracks()[0];
 
-  count_dialog.addEventListener("close", function(event) {
-    if (this.returnValue === "default") {
-      // Form was submitted
-      const upc = out.textContent;
-      if (inventory[upc] === undefined) inventory[upc] = 0;
-      inventory[upc] += Number(count.value);
-    }
-    scan_btn.disabled = false;
-    cam.play();
-    count.value = "";
-  });
+		Object.assign(cam, { srcObject: stream, autoplay: true });
+		Object.assign(cam.style, {
+			width: `${height}px`,
+			height: `${width}px`,
+			marginLeft: `calc(50vw - ${height / 2}px)`
+		});
+		isStarted = true;
 
-  settings_dialog.addEventListener("close", function(event) {
-    const rval = this.returnValue;
-    if (rval === "cancel") return;
-    if (rval === "export") {
-      const type = export_type.value;
-      if (type === "csv") {
-        saveAs(`upc,count\r\n${JSON.stringify(inventory).slice(1).slice(0,-1).replaceAll('"',"").split(",").map(item => item.replace(":", ",")).join("\r\n")}`, "count.csv", "text/csv");
-        inventory = {};
-      }
-      return;
-    }
-    localStorage.beep_tone = beep_tone.value;
-    localStorage.beep_time = beep_time.valueAsNumber / 1000;
-    localStorage.beep_vol = beep_vol.valueAsNumber / 100;
-  });
+		scan_btn.disabled = false;
+		light.disabled = false;
+	})
+	.catch(err => alert(err));
 
-  // Update UI with user-defined settings, if set
-  if (localStorage.beep_tone) beep_tone.value = localStorage.beep_tone;
-  if (localStorage.beep_time) beep_time.valueAsNumber = localStorage.beep_time;
-  if (localStorage.beep_vol) beep_vol.valueAsNumber = localStorage.beep_vol;
+	// Toggle the flashlight
+	light.addEventListener("click", event => {
+		const target = event.target;
 
-  // Play the beep tone whenever they change the settings for it
-  beep_tone.onchange = beep;
-  beep_time.onchange = beep;
-  beep_vol.onchange = beep;
+		track.applyConstraints({
+			advanced: [{ torch: (target.textContent === "Light On") }]
+		}).then(() => {
+			target.textContent = "Light Off";
+		}).catch(err => alert(err));
+	});
 
-  function scan() {
-    // Detect barcodes
-    detector
-    .detect(cam)
-    .then(barcodes => {
-      if (barcodes.length < 1) {
-        requestAnimationFrame(scan);
-        return;
-      }
-      let captured = false;
-      /**
-       * barcode.boundingBox = Dimensions of a rectangle representing the detected barcode
-       * barcode.cornerPoints = The x and y coordinates of the four corners of the barcode relative to the image starting from the top-left
-       * barcode.format = The detected barcode format
-       * barcode.rawValue = A string decoded from the barcode data
-       */
-      const barcode = barcodes[0],
-            format = barcode.format;
-      let data = barcode.rawValue;
+	scan_btn.addEventListener("click", event => {
+		scan_btn.disabled = true;
+		scan();
+	});
 
-      // UPC-A / UPC-E
-      if (format === "upc_a" && isValidUPC(data)) {
-        beep();
-        getCount(data);
-        captured = true;
-      }
+	count_dialog.addEventListener("close", function(event) {
+		if (this.returnValue === "default") {
+			// Form was submitted
+			const upc = out.textContent;
+			if (inventory[upc] === undefined) inventory[upc] = 0;
+			inventory[upc] += Number(count.value);
+		}
+		scan_btn.disabled = false;
+		cam.play();
+		count.value = "";
+	});
 
-      if (format === "upc_e") {
-        data = expandUPC(data);
-        if (isValidUPC(data)) {
-          beep();
-          getCount(data);
-          captured = true;
-        }
-      }
+	settings_dialog.addEventListener("close", function(event) {
+		const rval = this.returnValue;
+		if (rval === "cancel") return;
+		if (rval === "export") {
+			const type = export_type.value;
+			if (type === "csv") {
+				const inventoryString = JSON.stringify(inventory)
+																		.slice(1).slice(0,-1)
+																		.replaceAll('"',"")
+																		.split(",")
+																		.map(item => item.replace(":", ","))
+																		.join("\r\n");
+				saveAs(`upc,count\r\n${inventoryString}`, "count.csv", "text/csv");
+				inventory = {};
+			}
+			return;
+		}
+		localStorage.beep_tone = beep_tone.value;
+		localStorage.beep_time = beep_time.valueAsNumber / 1000;
+		localStorage.beep_vol = beep_vol.valueAsNumber / 100;
+	});
 
-      // QR Code
-      if (format === "qr_code") {
-        beep();
-        captured = true;
-        scan_btn.disabled = false;
-        // If the data is a web address, ask to open it
-        if (/^https?:\/\//.test(data)) {
-          cam.pause();
-          if (confirm(`Would you like to open '${data}' in a new tab?`)) {
-            open(data, "_blank", "noreferrer");
-          } else {
-            cam.play();
-          }
-        }
-        // If it's only numbers, treat it like a UPC
-        if (/^[0-9]*$/.test(data) && isValidUPC(data)) {
-          getCount(data);
-        }
-      }
+	// Update UI with user-defined settings, if set
+	if (localStorage.beep_tone) beep_tone.value = localStorage.beep_tone;
+	if (localStorage.beep_time) beep_time.valueAsNumber = localStorage.beep_time;
+	if (localStorage.beep_vol) beep_vol.valueAsNumber = localStorage.beep_vol;
 
-      // Scan until a barcode is captured, then enable the scan button
-      if (!captured) requestAnimationFrame(scan);
-    }).catch(err => alert(err));
-  }
+	// Play the beep tone whenever they change the settings for it
+	beep_tone.onchange = beep;
+	beep_time.onchange = beep;
+	beep_vol.onchange = beep;
 
-  // Calculates the check digit of a UPC-A number and validates it against the last digit
-  function isValidUPC(upc) {
-    if (upc.length !== 12) throw RangeError("UPC must be exactly 12 digits long");
-    const split = String(upc).split('').map(digit => Number(digit)),
-          checkDigit = (((split[0] + split[2] + split[4] + split[6] + split[8] + split[10]) * 3) + split[1] + split[3] + split[5] + split[7] + split[9]) % 10;
-    return Number(upc[11]) === (checkDigit === 0 ? 0 : 10 - checkDigit);
-  }
+	function scan() {
+		// Detect barcodes
+		detector
+		.detect(cam)
+		.then(barcodes => {
+			if (barcodes.length < 1) {
+				requestAnimationFrame(scan);
+				return;
+			}
+			let captured = false;
+			/**
+			 * barcode.boundingBox = Dimensions of a rectangle representing the detected barcode
+			 * barcode.cornerPoints = The x and y coordinates of the four corners of the barcode relative to the image
+			 *                        starting from the top-left
+			 * barcode.format = The detected barcode format
+			 * barcode.rawValue = A string decoded from the barcode data
+			 */
+			const barcode = barcodes[0],
+						format = barcode.format;
+			let data = barcode.rawValue;
 
-  // Expands UPC-E barcode to UPC-A format
-  function expandUPC(upce) {
-    if (typeof upce !== "string") upce = `${upce}`;
-    let len = upce.length;
-    if (len !== 8) throw Error(`UPC must be exactly 8 digits`);
+			// UPC-A / UPC-E
+			if (format === "upc_a" && isValidUPC(data)) {
+				beep();
+				getCount(data);
+				captured = true;
+			}
 
-    // Extract prefix, assume zero if omitted
-    let prefix = "0";
-    if (len > 6) {
-      prefix = upce[0];
-      upce = upce.slice(1);
-      len--;
-    }
+			if (format === "upc_e") {
+				data = expandUPC(data);
+				if (isValidUPC(data)) {
+					beep();
+					getCount(data);
+					captured = true;
+				}
+			}
 
-    // Extract Check Digit
-    let checkDigit;
-    if (len > 6) {
-      checkDigit = upce.slice(-1);
-      upce = upce.slice(0, -1);
-      len--;
-    }
+			// QR Code
+			if (format === "qr_code") {
+				beep();
+				captured = true;
+				scan_btn.disabled = false;
+				// If the data is a web address, ask to open it
+				if (/^https?:\/\//.test(data)) {
+					cam.pause();
+					if (confirm(`Would you like to open '${data}' in a new tab?`)) {
+						open(data, "_blank", "noreferrer");
+					} else {
+						cam.play();
+					}
+				}
+				// If it's only numbers, treat it like a UPC
+				if (/^[0-9]*$/.test(data) && isValidUPC(data)) {
+					getCount(data);
+				}
+			}
 
-    let upca = `${prefix}${upce[0]}${upce[1]}`;
-    const TRI = `${upce[2]}${upce[3]}${upce[4]}`;
-    switch (upce[5]) {
-      case "0":
-      case "1":
-      case "2":
-        upca += `${upce[5]}0000${TRI}`;
-        break;
-      case "3":
-        upca += `${upce[2]}00000${upce[3]}${upce[4]}`;
-        break;
-      case "4":
-        upca += `${upce[2]}${upce[3]}00000${upce[4]}`;
-        break;
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        upca += `${TRI}0000${upce[5]}`;
-        break;
-      default:
-        break;
-    }
-    return `${upca}${checkDigit}`;
-  }
+			// Scan until a barcode is captured, then enable the scan button
+			if (!captured) requestAnimationFrame(scan);
+		}).catch(err => alert(err));
+	}
 
-  function getCount(upc) {
-    out.textContent = upc;
-    total.textContent = `Total: ${inventory[upc] || 0}`;
-    cam.pause();
-    count_dialog.showModal();
-  }
+	// Validity check is very basic. There's ~11.11111...% chance of a false positive
+	const isValidUPC = upc => {
+		if (typeof upc !== "string") throw TypeError("Input must be a String");
+		if (upc.length !== 12) throw RangeError("UPC must be exactly 12 digits long");
+		const nums = upc.split(/:?/).map(digit => parseInt(digit));
+		return (((3*nums[0]) + nums[1] + (3*nums[2]) + nums[3] + (3*nums[4]) + nums[5] + (3*nums[6]) + nums[7] + (3*nums[8]) + nums[9]) % 10) === 0;
+	};
 
-  // Play a beep noise
-  function beep() {
-    const ctx = new AudioContext(),
-          osc = ctx.createOscillator(),
-          gainNode = ctx.createGain();
+	// Expands UPC-E barcode to UPC-A format
+	function expandUPC(upce) {
+		if (typeof upce !== "string") upce = `${upce}`;
+		let len = upce.length;
+		if (len !== 8) throw Error(`UPC must be exactly 8 digits`);
 
-    // Connect oscillator and gain nodes
-    osc.connect(gainNode);
-    gainNode.connect(ctx.destination);
+		// Extract prefix, assume zero if omitted
+		let prefix = "0";
+		if (len > 6) {
+			prefix = upce[0];
+			upce = upce.slice(1);
+			len--;
+		}
 
-    // Set volume
-    gainNode.gain.value = beep_vol.valueAsNumber / 100;
+		// Extract Check Digit
+		let checkDigit;
+		if (len > 6) {
+			checkDigit = upce.slice(-1);
+			upce = upce.slice(0, -1);
+			len--;
+		}
 
-    // Set wave type and frequency
-    osc.type = "square";
-    osc.frequency.value = beep_tone.value;
+		let upca = `${prefix}${upce[0]}${upce[1]}`;
+		const TRI = `${upce[2]}${upce[3]}${upce[4]}`;
+		switch (upce[5]) {
+			case "0":
+			case "1":
+			case "2":
+				upca += `${upce[5]}0000${TRI}`;
+				break;
+			case "3":
+				upca += `${upce[2]}00000${upce[3]}${upce[4]}`;
+				break;
+			case "4":
+				upca += `${upce[2]}${upce[3]}00000${upce[4]}`;
+				break;
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+				upca += `${TRI}0000${upce[5]}`;
+				break;
+			default:
+				break;
+		}
+		return `${upca}${checkDigit}`;
+	}
 
-    // Disconnect audio nodes when finished playing
-    osc.onended = () => {
-      osc.disconnect();
-      gainNode.disconnect();
-    };
+	function getCount(upc) {
+		out.textContent = upc;
+		total.textContent = `Total: ${inventory[upc] || 0}`;
+		cam.pause();
+		count_dialog.showModal();
+	}
 
-    // Play sound
-    osc.start();
-    osc.stop(beep_time.valueAsNumber / 1000);
-  }
+	// Play a beep noise
+	function beep() {
+		const ctx = new AudioContext(),
+					osc = ctx.createOscillator(),
+					gainNode = ctx.createGain();
 
-  // Creates a file save prompt for a given input
-  const saveAs = (data, filename = "untitled", type = "text/plain") => {
-    if (typeof data !== "string") throw TypeError("Input data must be of type String");
-    const url = window.URL.createObjectURL(new Blob([data], { type }));
-    const a = document.createElement('a');
-    a.download = filename;
-    a.href = url;
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  };
-})();
+		// Connect oscillator and gain nodes
+		osc.connect(gainNode);
+		gainNode.connect(ctx.destination);
+
+		// Set volume
+		gainNode.gain.value = beep_vol.valueAsNumber / 100;
+
+		// Set wave type and frequency
+		osc.type = "square";
+		osc.frequency.value = beep_tone.value;
+
+		// Disconnect audio nodes when finished playing
+		osc.onended = () => {
+			osc.disconnect();
+			gainNode.disconnect();
+		};
+
+		// Play sound
+		osc.start();
+		osc.stop(beep_time.valueAsNumber / 1000);
+	}
+
+	// Creates a file save prompt for a given input
+	const saveAs = (data, filename = "untitled", type = "text/plain") => {
+		if (typeof data !== "string") throw TypeError("Input data must be of type String");
+		showSaveFilePicker().then(async handle => {
+			const stream = await handle.createWritable({
+				suggestedName: filename,
+				types: [{
+					description: "Text File",
+					accept: {
+						"text/plain": [".txt"]
+					}
+				}]
+			});
+			await stream.write(new Blob([data], { type }));
+			await stream.close();
+		});
+	};
+});
--- sw.js
+++ sw.js
@@ -1,64 +1,52 @@
 const APP_PREFIX = "phoenix",
-      APP_VERSION = "1.0.1",
-      CACHE_NAME = `${APP_PREFIX}_v${APP_VERSION}`;
+			APP_VERSION = "1.0.2",
+			CACHE_NAME = `${APP_PREFIX}_v${APP_VERSION}`;
 
 let APP_URLS = [
-  location.pathname,
-  "index.html",
-  "credits.html",
-  "app.js",
-  "css/app.css",
-  "images/favicon.ico",
-  "images/favicon-16x16.png",
-  "images/favicon-32x32.png",
-  "images/android-chrome-192x192.png",
-  "images/android-chrome-512x512.png",
-  "images/favicon.svg"
+	location.pathname,
+	"index.html",
+	"credits.html",
+	"app.js",
+	"css/app.css",
+	"images/favicon.ico",
+	"images/favicon-16x16.png",
+	"images/favicon-32x32.png",
+	"images/android-chrome-192x192.png",
+	"images/android-chrome-512x512.png",
+	"images/favicon.svg"
 ];
-
-let _platform = navigator.platform;
-if (_platform.startsWith("Win")) {
-  APP_URLS.push("browserconfig.xml");
-  APP_URLS.push("images/mstile-150x150.png");
-}
-
-if (_platform === "iPhone" || _platform === "iPad") {
-  APP_URLS.push("images/apple-touch-icon-precomposed.png");
-  APP_URLS.push("images/apple-touch-icon.png");
-}
-_platform = null;
 
 // Cache APP_URLS when an install is requested
 self.addEventListener("install", event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.addAll(APP_URLS);
-  })());
+	event.waitUntil((async () => {
+		const cache = await caches.open(CACHE_NAME);
+		await cache.addAll(APP_URLS);
+	})());
 });
 
 // Provide from the cache before requesting from the network
 self.addEventListener("fetch", event => {
-  event.respondWith((async () => {
-    const req = event.request,
-          r = await caches.match(req);
-    if (r) return r;
-    const res = await fetch(req),
-          cache = await caches.open(CACHE_NAME);
-    cache.put(req, res.clone());
-    return res;
-  })());
+	event.respondWith((async () => {
+		const req = event.request,
+					r = await caches.match(req);
+		if (r) return r;
+		const res = await fetch(req),
+					cache = await caches.open(CACHE_NAME);
+		cache.put(req, res.clone());
+		return res;
+	})());
 });
 
 // Delete old versions from the cache
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => {
-    return Promise.all(keys.map(key => {
-      if (key === CACHE_NAME) return;
-      return caches.delete(key);
-    }));
-  }));
+	event.waitUntil(caches.keys().then(keys => {
+		return Promise.all(keys.map(key => {
+			if (key === CACHE_NAME) return;
+			return caches.delete(key);
+		}));
+	}));
 });
 
 self.addEventListener("message", event => {
-  if (event.data.action === "skipWaiting") self.skipWaiting();
+	if (event.data.action === "skipWaiting") self.skipWaiting();
 });
